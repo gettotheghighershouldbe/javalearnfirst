75882a56656ca6d61a088e555557b22dfeeed8e9
gettotheghighershouldbe <57565161+gettotheghighershouldbe@users.noreply.github.com>
2020-01-05 22:36:29 +0800

    2020-1-6change

---
+++ JavaHeadFirst/CompileCorrect.java
@@ -0,0 +1,32 @@
+public class CompileCorrect{
+	public static void main(String[] args){
+		/*int x = 1;
+		while(x<10){
+			if(x>3){
+				System.out.println("big" + x);
+			}
+			x = x + 1;
+		}*/
+
+		/*int x = 5;
+		while(x>1){
+			
+			if(x<3){
+				System.out.println("small x");
+			}
+			x = x-1;
+		}*/
+
+
+		int x = 5;
+		while(x>1){
+			
+			if(x<3){
+				System.out.println("small x");
+			}
+			x = x -1;
+		}
+
+
+	}
+}
