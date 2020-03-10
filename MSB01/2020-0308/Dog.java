816f14cd3903e5c4c5f623aa7f19be460c3f9206
gettotheghighershouldbe <57565161+gettotheghighershouldbe@users.noreply.github.com>
2020-03-10 08:48:25 +0800

    0310 1stchange

---
+++ MSB01/2020-0308/Dog.java
@@ -0,0 +1,22 @@
+public class Dog {
+	int furColor;
+	int height;
+
+	void catchMouse (Mouse m) {
+		//ssssassds
+	}
+
+
+
+
+public static void main (String[] args) {
+
+	Dog d = new Dog();
+	Mouse m = new Mouse();
+	d.catchMouse(m);
+	HelloWorld hw = nee HelloWorld();
+
+}
+}
+
+class Mouse {}
